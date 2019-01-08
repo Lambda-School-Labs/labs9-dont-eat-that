9ba0c3f429e734b6b0afe5b30c934b769e24198e
Edward J <edjeong99@gmail.com>
2019-01-08 18:08:55 -0500

    add dependencies and edit index.js to start a store

---
+++ client/src/reducers/index.js
@@ -0,0 +1,25 @@
+
+const initialState = {
+    fetching: false,
+    smurfs: [],
+    error: null
+  };
+  
+  export const RecipesReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case "FETCHING_REQUEST":
+        return { ...state, fetching: true };
+  
+      case "FETCHING_SUCCESS":
+        console.log("fetching success", action.payload);
+        return { ...state, fetching: false, smurfs: [...action.payload] };
+  
+      case "FETCHING_FAILURE":
+        return { ...state, fetching: false, error: action.payload };
+  
+
+      default:
+        return state;
+    }
+  };
+  
