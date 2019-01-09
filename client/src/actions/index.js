004c646a3a7d07a15cd9439402d710fa2aea4ed4
vanceleon <vanceleon44@gmail.com>
2019-01-09 18:32:54 -0500

    adding single recipe get edit display list recipe and single recipe component to work with backend data

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,10 +4,12 @@ export const ERROR = 'ERROR';
 export const GET_RECIPES = 'GET_RECIPES';
 export const GETTING_RECIPES = 'GETTING_RECIPES';
 
-// const URL = "" this is backend
+const URL = "https://donteatthat.herokuapp.com/"
 
+
+// all recipes
 export const getRecipes = () => {
-  const recipes = axios.get(URL);
+  const recipes = axios.get(`${URL}api/recipes/all`);
   return dispatch => {
     dispatch({ type: GETTING_RECIPES });
     recipes
@@ -19,3 +21,19 @@ export const getRecipes = () => {
       });
   };
 };
+
+
+// single recipe
+export const getRecipes = () => {
+    const recipes = axios.get(`${URL}api/recipes/all`);
+    return dispatch => {
+      dispatch({ type: GETTING_RECIPES });
+      recipes
+        .then(response => {
+          dispatch({ type: GET_RECIPES, payload: response.data });
+        })
+        .catch(err => {
+          dispatch({ type: ERROR, payload: err });
+        });
+    };
+  };
