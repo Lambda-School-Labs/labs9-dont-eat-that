a33e91f3f00f04125987fefc610a041dde878d7a
Edward J <edjeong99@gmail.com>
2019-01-09 11:57:06 -0500

    fix client side (commented out codes) so it would be publishing

--- client/src/App.js
+++ client/src/App.js
@@ -10,16 +10,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import SideMenu from "./components/sideMenu.js";
-import TopMenu from "./components/topMenu.js";
+// import SideMenu from "./components/sideMenu.js";
+// import TopMenu from "./components/topMenu.js";
+import DisplayRecipesViewer from "./viewer/DisplayRecipesViewer.js";
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <TopMenu />
-        <SideMenu />
-        <DisplayRecipesViewer />
+
+      Welcome to Don't Eat That
+        {/* <TopMenu /> */}
+        {/* <SideMenu /> */}
+        {/* <DisplayRecipesViewer /> */}
       </div>
     );
   }
--- client/src/viewer/DisplayRecipesViewer.js
+++ client/src/viewer/DisplayRecipesViewer.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
-import {
-  DeleteRecipe,
-  DisplayListRecipes,
-  DisplayOneRecipe,
-  EditRecipe,
-  Login,
-  Nutrition,
-  UserSettings,
-  Sidebar,
-  SearchBar
-} from '../components';
+// import {
+//   DeleteRecipe,
+//   DisplayListRecipes,
+//   DisplayOneRecipe,
+//   EditRecipe,
+//   Login,
+//   Nutrition,
+//   UserSettings,
+//   Sidebar,
+//   SearchBar
+// } from '../components';
 
 // this will be for the actions
 // import {
@@ -23,45 +23,45 @@ class DisplayRecipeViewer extends Component {
     // getting all the notes function will go here
   }
 
-  render() {
-    return (
-      <div className="recipe-view-container">
-        <header>
-          <Route
-            path="/"
-            render={props => {
-              <Login />;
-            }}
-          />
-          <Route
-            path="/"
-            render={props => {
-              <SearchBar />;
-            }}
-          />
-        </header>
-        <div className="recipe-body">
-          <main className="recipe-list">
-          <Route
-            path="/"
-            render={props => {
-              <DisplayListRecipes />;
-            }}
-          />
-          </main>
-          <nav>
-          <Route
-            path="/"
-            render={props => {
-              <Sidebar />;
-            }}
-          />  
-          </nav>
-        </div>
-        <footer>license, disclaimers, links to profiles are here</footer>
-      </div>
-    );
-  }
+  // render() {
+  //   return (
+  //     <div className="recipe-view-container">
+  //       <header>
+  //         <Route
+  //           path="/"
+  //           render={props => {
+  //             <Login />;
+  //           }}
+  //         />
+  //         <Route
+  //           path="/"
+  //           render={props => {
+  //             <SearchBar />;
+  //           }}
+  //         />
+  //       </header>
+  //       <div className="recipe-body">
+  //         <main className="recipe-list">
+  //         <Route
+  //           path="/"
+  //           render={props => {
+  //             <DisplayListRecipes />;
+  //           }}
+  //         />
+  //         </main>
+  //         <nav>
+  //         <Route
+  //           path="/"
+  //           render={props => {
+  //             <Sidebar />;
+  //           }}
+  //         />  
+  //         </nav>
+  //       </div>
+  //       <footer>license, disclaimers, links to profiles are here</footer>
+  //     </div>
+  //   );
+  // }
 }
 
 export default DisplayRecipeViewer;
