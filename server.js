bb18eb5be24a37ecd5aa76157651c82f238222f6
Peter Pham <phamhere@gmail.com>
2019-01-08 17:23:25 -0700

    setup recipeRouter and userRouter, made three routes for all recipes, user recipes, and singular recipe

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ const express = require('express');
 const knex = require('knex');
 const cors = require('cors');
 
+const recipeRouter = require('./routes/recipeRouter');
+
 const server = express();
 
+server.use(express.json());
 server.use(cors());
 
-server.get('/', (req,res) => res.send('Welcome to the Don\'t Eat That app server!'));
+server.use('/api/recipes/', recipeRouter);
+
+server.get('/', (req, res) =>
+  res.send("Welcome to the Don't Eat That app server!")
+);
 
 module.exports = server;
