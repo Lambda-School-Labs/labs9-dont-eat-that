ab7fc3b5a314d587d258a1f714701930d1779ada
Peter Pham <phamhere@gmail.com>
2019-01-09 10:47:37 -0700

    fixed GET single recipe

--- routes/recipeRouter.js
+++ routes/recipeRouter.js
@@ -31,28 +31,58 @@ router.get('/:userid', (req, res) => {
 });
 
 // geting single recipe details
-router.get('/one/:id', (req, res) => {
-  const id = req.params.id;
-  db('recipes')
-    .where({ id: id })
-    .first()
-    .join('recipes-ingredients', 'recipes.id', 'recipes-ingredients.recipe_id')
-    .join('ingredients', 'ingredients.id', 'recipes-ingredients.ingredient_id')
-    .select(
-      knex.ref('recipes.name').as('recipename'),
-      'recipes.description',
-      'recipes.user_id',
-      knex.ref('ingredients.name').as('ingredientname'),
-      'recipes-ingredients.quantity',
-      'ingredients.unit'
-    )
-    .then(recipe => res.status(200).json(recipe))
-    .catch(err =>
-      res.status(500).json({
-        message: 'The recipes information could not be retrieved',
-        err
-      })
-    );
+// router.get('/one/:id', (req, res) => {
+//   const id = req.params.id;
+//   db('recipes')
+//     .where({ 'recipes.id': id })
+//     .first()
+//     .join('recipes-ingredients', 'recipes.id', 'recipes-ingredients.recipe_id')
+//     .join('ingredients', 'ingredients.id', 'recipes-ingredients.ingredient_id')
+//     .select(
+//       db.ref('recipes.name').as('recipename'),
+//       'recipes.description',
+//       'recipes.user_id',
+//       db.ref('ingredients.name').as('ingredientname'),
+//       'recipes-ingredients.quantity',
+//       'ingredients.unit'
+//     )
+//     .then(recipe => res.status(200).json(recipe))
+//     .catch(err => {
+//       console.log(err);
+//       res.status(500).json({
+//         message: 'The recipes information could not be retrieved',
+//         err
+//       });
+//     });
+// });
+
+router.get('/one/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const recipe = await db('recipes')
+      .where({ id: id })
+      .first();
+    const ingredients = await db('ingredients')
+      .join(
+        'recipes-ingredients',
+        'ingredients.id',
+        'recipes-ingredients.ingredient_id'
+      )
+      .join('recipes', 'recipes.id', 'recipes-ingredients.recipe_id')
+      .where({ 'recipes.id': id })
+      .select(
+        'ingredients.name',
+        'recipes-ingredients.quantity',
+        'ingredients.unit'
+      );
+    res.status(200).json({ ...recipe, ingredients: ingredients });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'The recipes information could not be retrieved',
+      err
+    });
+  }
 });
 
 module.exports = router;
