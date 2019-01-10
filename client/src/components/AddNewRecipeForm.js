04dd9bbf30adda5ddc59fd205a5bad11839f787d
MEBonham <programmer@mckaybonham.com>
2019-01-10 15:03:12 -0700

    Ingredients form fields mostly working

--- client/src/components/AddNewRecipeForm.js
+++ client/src/components/AddNewRecipeForm.js
@@ -5,7 +5,9 @@ export default class AddNewRecipeForm extends Component {
         super(props);
         this.state = {
             name: "",
-            description: ""
+            description: "",
+            numIngredients: 3,
+            ingredients: []
         };
     }
 
@@ -15,12 +17,93 @@ export default class AddNewRecipeForm extends Component {
         });
     }
 
+    ingHandler = ev => {
+        let rowType = ev.target.name.slice(0, 4);
+        if (rowType === "quty") {
+            rowType = 'quantity';
+        }
+        let rowNum = Number(ev.target.name.slice(4));
+        if (rowNum >= this.state.ingredients.length) {
+            let newObj = { [rowType]: ev.target.value };
+            this.setState({
+                ingredients: [
+                    ...this.state.ingredients,
+                    newObj
+                ]
+            });
+            if (rowNum > this.state.ingredients.length) {
+                ev.target.blur();
+            }
+        } else {
+            let ingArray = this.state.ingredients;
+            let oldObj = ingArray[rowNum];
+            let newObj = {
+                ...oldObj,
+                [rowType]: ev.target.value
+            };
+            ingArray[rowNum] = newObj;
+            this.setState({
+                ingredients: ingArray
+            });
+        }
+    }
+
     submitHandler = ev => {
         ev.preventDefault();
-        console.log("Got here, now need to do axios stuff");
+        console.log("Got here, now need to call an action");
     }
 
     render() {
+        let ingredientRows = [];
+        for (let i = 0; i < this.state.numIngredients; i++) {
+            if (i >= this.state.ingredients.length) {
+                ingredientRows.push(<input 
+                    type="text" 
+                    placeholder="Ingredient Name" 
+                    name={`name${i}`} 
+                    value="" 
+                    onChange={this.ingHandler} 
+                />);
+                ingredientRows.push(<input 
+                    type="text" 
+                    placeholder="Ingredient Quantity" 
+                    name={`quty${i}`} 
+                    value=""
+                    onChange={this.ingHandler} 
+                />);
+                ingredientRows.push(<input 
+                    type="text" 
+                    placeholder="Ingredient Unit" 
+                    name={`unit${i}`} 
+                    value="" 
+                    onChange={this.ingHandler} 
+                />);
+                ingredientRows.push(<br />);
+            } else {
+                ingredientRows.push(<input 
+                    type="text" 
+                    placeholder="Ingredient Name" 
+                    name={`name${i}`} 
+                    value={this.state.ingredients[i].name}
+                    onChange={this.ingHandler} 
+                />);
+                ingredientRows.push(<input 
+                    type="text" 
+                    placeholder="Ingredient Quantity" 
+                    name={`quty${i}`} 
+                    value={this.state.ingredients[i].quantity} 
+                    onChange={this.ingHandler} 
+                />);
+                ingredientRows.push(<input 
+                    type="text" 
+                    placeholder="Ingredient Unit" 
+                    name={`unit${i}`} 
+                    value={this.state.ingredients[i].unit} 
+                    onChange={this.ingHandler} 
+                />);
+                ingredientRows.push(<br />);
+            }
+        }
         return (
             <form onSubmit={this.submitHandler}>
                 <h2>Upload New Recipe</h2>
@@ -39,6 +122,15 @@ export default class AddNewRecipeForm extends Component {
                     onChange={this.typingHandler}
                     required
                 />
+                <input
+                    type="number"
+                    placeholder="3"
+                    name="numIngredients"
+                    value={this.state.numIngredients}
+                    onChange={this.typingHandler}
+                />
+                <br />
+                {ingredientRows}
                 <button type="submit">Save Recipe</button>
             </form>
         );
