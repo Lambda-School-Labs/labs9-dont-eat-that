b351c75dc9c967b88a8426a61f6a60d63be772a0
Peter Pham <phamhere@gmail.com>
2019-01-09 17:50:34 -0700

    configuring back-end to work with heroku postgresql

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,17 @@
-// Update with your config settings.
+const localPgConnection = {
+  host: "localhost",
+  database: "lambda",
+  user: "peter",
+  password: "pass"
+};
+
+const dbConnection = process.env.DATABASE_URL || localPgConnection;
 
 module.exports = {
-
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: './data/dev.sqlite3'
+      filename: "./data/dev.sqlite3"
     },
     useNullAsDefault: true,
     migrations: {
@@ -16,19 +22,17 @@ module.exports = {
     }
   },
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: "pg",
+    connection: dbConnection,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
     }
   }
-
 };
