d0a2a63830b1e89cd94f6edb12dca39dfeb25dde
MEBonham <programmer@mckaybonham.com>
2019-01-09 18:02:16 -0700

    split reducers file into index.js and recipeReducer.js, renamed one action type

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 export const ERROR = 'ERROR';
 export const GET_RECIPES = 'GET_RECIPES';
 export const GETTING_RECIPES = 'GETTING_RECIPES';
+export const RECIPE_SUCCESS = 'RECIPE_SUCCESS';
+export const RECIPE_FAILURE = 'RECIPE_FAILURE';
 
 const URL = "https://donteatthat.herokuapp.com/"
 
@@ -22,18 +24,17 @@ export const getRecipes = () => {
   };
 };
 
-
 // single recipe
-// export const getRecipe = () => {
-//     const recipes = axios.get(`${URL}api/recipes/all`);
-//     return dispatch => {
-//       dispatch({ type: GETTING_RECIPE });
-//       recipes
-//         .then(response => {
-//           dispatch({ type: GET_RECIPES, payload: response.data });
-//         })
-//         .catch(err => {
-//           dispatch({ type: ERROR, payload: err });
-//         });
-//     };
-//   };
+export const getRecipe = id => {
+    const recipes = axios.get(`${URL}api/recipes/one/${id}`);
+    return dispatch => {
+      dispatch({ type: GETTING_RECIPES });
+      recipes
+        .then(response => {
+          dispatch({ type: GET_RECIPES, payload: response.data });
+        })
+        .catch(err => {
+          dispatch({ type: ERROR, payload: err });
+        });
+    };
+  };
