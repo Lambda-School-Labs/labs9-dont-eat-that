798ff5fbbade1f5b70a6448a94154983d993cfc3
Edward J <edjeong99@gmail.com>
2019-01-10 16:03:27 -0500

    finish server testing - need to fix a few bug

--- _test_/recipeRouter.test.js
+++ _test_/recipeRouter.test.js
@@ -4,9 +4,10 @@ const server = require("../server.js");
 
 const recipeRouterURL = "/api/recipes";
 
+// like to delete test recipe but do not know the recipe id to delete it
 // beforeEach(async () => {
-//     const temp = await request(server).delete("/games");
-//   });
+//     const temp = await request(server).delete(recipeRouterURL+"delete");
+//    });
 
   describe("/ route", () => {
     it("should return status code 200", async () => {
@@ -104,3 +105,132 @@ const localURL = "/all";
     });
 
   })
+
+
+  // test deleting a recipe with recipe id
+// need to check/change id before testing...
+
+  describe("/api/recipes/delete  DELETE", () => {
+    
+    // change recipe ID that you want to delete
+    const localURL = "/delete/35";
+   
+
+
+    it("should return status code 200, JSON and have property of name", async () => {
+      let response = await request(server).delete(recipeRouterURL+localURL);
+
+        expect(response.status).toBe(200);
+    
+    
+      expect(response.type).toBe("application/json");
+
+      expect(response.body).toBe(1);
+      
+    });
+
+    // if wrong id is used to delete
+    it("should return status code 404 and has property message" , async () => {
+      let response = await request(server).delete(recipeRouterURL+"delete/987");
+
+        expect(response.status).toBe(404);
+    
+    
+      
+    });
+
+  })
+
+// testing edit endpoint
+// use recipe id #10 for testing
+//   // currently having error due to local DB is sqlite3 and not PostgreSQL
+
+/*
+  describe("/api/recipes/edit/:id PUT", () => {
+    const localURL = "/edit/10";
+   const input = { 
+      name: "Edited name",  
+    description: "test descr", 
+    userid : 856, 
+    ingredients:"test ingred"
+  };
+     
+
+    it("should return status code 200", async () => {
+      let response = await request(server)
+      .put(recipeRouterURL+localURL)
+      .send(input);
+   
+      expect(response.status).toBe(200);
+    });
+  
+    it("should return json response type ", async () => {
+       let response = await request(server)
+       .put(recipeRouterURL+localURL)
+        .send(input);
+
+      expect(response.type).toBe("application/json");
+    });
+  
+    it('should return defined object', async () => {
+       let response = await request(server)
+       .put(recipeRouterURL+localURL)
+       .send(input);
+
+      expect(response.body).toBeDefined();
+      expect(response.body).toHaveProperty("name");
+      
+    });
+   
+
+  })
+
+*/
+
+  
+  // testing create endpoint
+  // currently having error due to local DB is sqlite3 and not PostgreSQL
+// will modify later
+  
+/*
+  describe("/api/recipes/create  POST", () => {
+    const localURL = "/create";
+
+    const input = { 
+      "name": "Ace5",  
+    "description": "test descr", 
+    "userid" : 856, 
+    "ingredients":"test ingred"
+  };
+   
+
+    it("should return status code 200", async () => {
+      let response = await request(server)
+      .post(recipeRouterURL+localURL)
+      .send(input);
+   
+      expect(response.status).toBe(200);
+    });
+  
+    it("should return json response type ", async () => {
+       let response = await request(server)
+       .post(recipeRouterURL+localURL)
+        .send(input);
+
+      expect(response.type).toBe("application/json");
+    });
+  
+    it('should return defined object', async () => {
+       let response = await request(server)
+       .post(recipeRouterURL+localURL)
+       .send(input);
+
+      expect(response.body).toBeDefined();
+      expect(response.body).toHaveProperty("name");
+      
+    });
+   
+
+  })
+
+  */
