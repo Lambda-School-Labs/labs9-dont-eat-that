124b0193af0256516cc699ef6a23af9f8f748375
Peter Pham <phamhere@gmail.com>
2019-01-12 11:52:41 -0700

    switched from Edward's private firebase to shared firebase

--- client/src/components/firebase/firebase.js
+++ client/src/components/firebase/firebase.js
@@ -1,41 +1,36 @@
 // NEED TO HIDE below data
 // use .env and add .env to gitignore
 
-
 import app from 'firebase/app';
 import 'firebase/auth';
 
 const config = {
-    apiKey: 'AIzaSyBZCPki28TLNa9O4pbwWvHQeG0_oZD-OEs',
-    authDomain: 'don-teatthat.firebaseapp.com',
-    databaseURL: 'https://don-teatthat.firebaseio.com',
-    projectId: 'don-teatthat',
-    storageBucket: 'don-teatthat.appspot.com',
-    messagingSenderId: '230245850928'
-  };
+  apiKey: 'AIzaSyB7ecoiT8cBV9IRX1n9Kt0UJZXvkpwfCSs',
+  authDomain: 'don-t-eat-that-97fbd.firebaseapp.com',
+  databaseURL: 'https://don-t-eat-that-97fbd.firebaseio.com',
+  projectId: 'don-t-eat-that-97fbd',
+  storageBucket: 'don-t-eat-that-97fbd.appspot.com',
+  messagingSenderId: '120302586888'
+};
 
-  class Firebase {
-      constructor(){
-          app.initializeApp(config);
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
 
-          this.auth = app.auth();
-      }
-
-
-      doCreateUserWithEmailAndPassword = (email, password) =>
-      this.auth.createUserWithEmailAndPassword(email, password);
-
-      doSignInWithEmailAndPassword = (email, password) =>
-      this.auth.signInWithEmailAndPassword(email, password);
-
-      doSignOut = () => this.auth.signOut();
-      
-      doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
-
-      doPasswordUpdate = password =>
-        this.auth.currentUser.updatePassword(password);
-
-
+    this.auth = app.auth();
   }
 
-  export default Firebase;
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
+}
+
+export default Firebase;
