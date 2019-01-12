bacdeab423f5f0992a3d9ea4c1a7799bc209d43e
Peter Pham <phamhere@gmail.com>
2019-01-12 15:15:37 -0700

    added brackets around json GET all users and recipes to fix hopefully

--- routes/recipeRouter.js
+++ routes/recipeRouter.js
@@ -25,7 +25,7 @@ router.get('/all', async (req, res) => {
         );
       return { ...recipes, ingredients };
     });
-    res.status(200).json(recipesAndIng);
+    res.status(200).json({ recipesAndIng });
   } catch (err) {
     res.status(500).json({
       message: 'The recipes information could not be retrieved',
