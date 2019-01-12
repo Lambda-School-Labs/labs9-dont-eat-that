619acc6f874c86dc16d8f7094e64ba82b6bd4028
Peter Pham <phamhere@gmail.com>
2019-01-11 23:23:19 -0700

    back-end for stripe integration

---
+++ routes/paymentRouter.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const stripe = require('stripe')('sk_test_gkVUpZdQd5USCZLsKCjdal9R');
+
+const db = require('../data/dbConfig');
+
+const router = express.Router();
+
+router.post('/charge', async (req, res) => {
+  const { token, customerPlan, firebaseid } = req.body;
+  const plan = // getting actually stripe plan id for silver or gold plan
+    customerPlan === 'silver' ? 'plan_EKIEXJhyKqBTFd' : 'plan_EKIFbngvwjejux';
+  try {
+    const customer = await stripe.customers.create({
+      // creating customer
+      source: token
+    });
+    await db('users') // saving customer id to user db
+      .where({ firebaseid })
+      .update({ customerid: customer.id });
+    const subscription = await stripe.subscriptions.create({
+      // subscription customer to plan
+      customer: customer.id,
+      items: [{ plan }]
+    });
+    await db('users') // saving subscription id to user db
+      .where({ firebaseid })
+      .update({ subscriptionid: subscription.id });
+    res.status(200).json({ subscription });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: 'There was an error processing your subscription.' });
+  }
+});
+
+router.post('/cancel', async (req, res) => {
+  const { firebaseid } = req.body;
+  try {
+    const user = await db('users') // getting user in db
+      .where({ firebaseid })
+      .first();
+    const subscription = await stripe.subscriptions.del(user.subscriptionid); // cancelling subscription with user subscriptionid
+    res.status(200).json({ subscription });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: 'There was an error canceling your subscription.' });
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 
 const recipeRouter = require('./routes/recipeRouter');
 const userRouter = require('./routes/userRouter');
+const paymentRouter = require('./routes/paymentRouter');
 
 const server = express();
 
@@ -11,6 +12,7 @@ server.use(express.json());
 
 server.use('/api/recipes/', recipeRouter);
 server.use('/api/users/', userRouter);
+server.use('/api/payments/', paymentRouter);
 
 server.get('/', (req, res) =>
   res.status(200).json("Welcome to the Don't Eat That app server!")
