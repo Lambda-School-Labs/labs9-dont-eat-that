fccec9ebcb5f279b09e24327ae78e1fcb608de41
donteatthat <46534396+donteatthat@users.noreply.github.com>
2019-01-11 17:50:36 -0700

    update server.js to use cors more properly

--- server.js
+++ server.js
@@ -6,18 +6,8 @@ const userRouter = require('./routes/userRouter');
 
 const server = express();
 
-server.use(express.json());
 server.use(cors());
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
-  );
-  next();
-});
+server.use(express.json());
 
 server.use('/api/recipes/', recipeRouter);
 server.use('/api/users/', userRouter);
