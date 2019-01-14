d9af14fb40558c48177e71bebbe70d4d0693b638
vanceleon <vanceleon44@gmail.com>
2019-01-14 10:56:54 -0500

    adding payment user recipe actions files separating out the actions and will import them into the index.js actions file

---
+++ client/src/actions/paymentActions.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+export const CANCEL_SUB = 'CANCEL_SUB';
+export const CHARGE_USER = 'CHARGE_USER';
+
+const URL = 'https://donteatthat.herokuapp.com';
+
+export const chargeUser = (token, plan) => dispatch => {
+    const firebaseid = localStorage.getItem('uid');
+    axios
+      .post(`${URL}/api/payments/charge`, {
+        token: token.id,
+        customerPlan: plan,
+        firebaseid
+      })
+      .then(res => dispatch({ type: CHARGE_USER, payload: true }))
+      .catch(err => dispatch({ type: ERROR, payload: err }));
+  };
+  
+  export const cancelSubscription = () => dispatch => {
+    const firebaseid = localStorage.getItem('uid');
+    axios
+      .post(`${URL}/api/payments/cancel`, { firebaseid })
+      .then(res => dispatch({ type: CANCEL_SUB, payload: true }))
+      .catch(err => dispatch({ type: ERROR, payload: err }));
+  };
+  
+  
---
+++ client/src/actions/userActions.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+export const ADD_USER = 'ADD_USER';
+export const GET_USER = 'GET_USER';
+
+
+const URL = 'https://donteatthat.herokuapp.com';
+
+
+export const getUser = () => dispatch => {
+    let firebaseid = localStorage.getItem('uid');
+    axios
+      .get(`${URL}/api/users/one/${firebaseid}`)
+      .then(res => dispatch({ type: GET_USER, payload: res.data }))
+      .catch(err => dispatch({ type: ERROR, payload: err }));
+  };
+  
+  export const addUser = firebaseid => dispatch => {
+    axios
+      .post(`${URL}/api/users/create`, { firebaseid })
+      .then(res =>
+        dispatch({ type: ADD_USER, payload: { id: res.data, firebaseid } })
+      )
+      .catch(err => dispatch({ type: ERROR, payload: err }));
+  };
+  
