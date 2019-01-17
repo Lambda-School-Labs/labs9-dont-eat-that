fca0083d56e2b76071c0d84d98bd6c5c2095d536
Edward J <edjeong99@gmail.com>
2019-01-17 14:25:14 -0500

    a cosmetic change

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -42,7 +42,7 @@ router.get('/one/:id', async (req, res) => {
         .join('users', 'users.id', 'users-allergies.user_id')
         .where({ 'users.id': user.id })
         .select('allergies.name');
-      const allergiesArr = allergies.map(allergy => allergy.name);
+    
       const recipes = await db('recipes')
         .where({ user_id: user.id })
         .select('id', 'name', 'description');
