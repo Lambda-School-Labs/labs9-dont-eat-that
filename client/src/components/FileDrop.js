4d27df05890029b5fd367ba587590fc8a4e18e73
vanceleon <vanceleon44@gmail.com>
2019-01-17 20:10:03 -0500

    adding a filedrop component

---
+++ client/src/components/FileDrop.js
@@ -0,0 +1,14 @@
+// let React = require('react');
+// let Dropzone = require('react-dropzone');
+// let axios = require('axios');
+
+// const URL = 
+
+// exports = module.exports = React.createClass({
+//     _onDrop: function(files) {
+//         let file = files[0];
+
+//         axios.get()
+//     }
+// })
+
