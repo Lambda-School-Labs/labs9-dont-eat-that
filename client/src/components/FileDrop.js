ff597ffe2e2e1726f944cf6f71c3e4bee0af2703
vanceleon <vanceleon44@gmail.com>
2019-01-17 20:37:28 -0500

    latest, trying to find good way to post images

--- client/src/components/FileDrop.js
+++ client/src/components/FileDrop.js
@@ -12,3 +12,46 @@
 //     }
 // })
 
+// import React from 'react'
+// import classNames from 'classnames'
+// import Dropzone from 'react-dropzone'
+// import { connect } from 'react-redux';
+
+// class MyDropzone extends React.Component {
+//    onDrop = (acceptedFiles, rejectedFiles) => {
+//      // Do something with files
+     
+//    }
+
+//    render() {
+//     return (
+//       <Dropzone onDrop={this.onDrop}>
+//         {({getRootProps, getInputProps, isDragActive}) => {
+//           return (
+//             <div
+//               {...getRootProps()}
+//               className={classNames('dropzone', {'dropzone--isActive': isDragActive})}
+//             >
+//               <input {...getInputProps()} />
+//               {
+//                 isDragActive ?
+//                   <p>Drop files here...</p> :
+//                   <p>Try dropping some files here, or click to select files to upload.</p>
+//               }
+//             </div>
+//           )
+//         }}
+//       </Dropzone>
+//     );
+//   }
+// }
+
+// const mapStateToProps = state => {
+//     return true;
+// }
+
+// export default connect(mapStateToProps)(MyDropzone);
+
+
+// possible link to follow
+// https://spin.atomicobject.com/2018/09/13/file-uploader-react-typescript/
