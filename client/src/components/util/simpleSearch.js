b4f6f76939e30a31fa1af3c9e3a9185cd2c73194
Edward J <edjeong99@gmail.com>
2019-01-17 19:09:10 -0500

    add testing for userRouter and fix search bug (make case insensitive)

--- client/src/components/util/simpleSearch.js
+++ client/src/components/util/simpleSearch.js
@@ -1,13 +1,11 @@
-// SimpleSearchInput has an input field 
+// SimpleSearchInput has an input field
 // for Simple Search.
 // Simple Search looks for search keywords in recipe and ingredient
 // and returns matching recipes
 
-
 import React, { Component } from "react";
 
 class SimpleSearchInput extends Component {
-
   render() {
     return (
       <form className="searchNote">
