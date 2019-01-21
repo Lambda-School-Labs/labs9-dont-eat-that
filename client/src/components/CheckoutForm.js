638dbc799cb8654d4651a21df9b231d6f3931e2c
Peter Pham <phamhere@gmail.com>
2019-01-21 15:04:23 -0700

    styled billing page

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { CardElement, injectStripe } from 'react-stripe-elements';
 import { connect } from 'react-redux';
+import { Button, Header, Table, Icon, Segment } from 'semantic-ui-react';
+
 import { chargeUser, cancelSubscription, getUser } from '../actions';
 
 class CheckoutForm extends React.Component {
@@ -23,39 +25,149 @@ class CheckoutForm extends React.Component {
     if (this.props.complete)
       return (
         <div>
-          <h1>Purchase Complete</h1>
-          <button onClick={this.props.cancelSubscription}>
+          <Header as="h1">Purchase Complete</Header>
+          <Button onClick={this.props.cancelSubscription}>
             Cancel Subscription
-          </button>
+          </Button>
         </div>
       );
     return (
       <div className="checkout">
-        <p>Would you like to complete the purchase?</p>
-        <label htmlFor="silver">Silver Plan: $2.00 per month</label>
+        <Header>Don't Eat That Subscription Plans</Header>
+        <Table
+          definition
+          striped
+          compact
+          size="small"
+          collapsing
+          color="violet"
+          inverted
+          style={{ width: '95%', marginLeft: '2.5%', fontFamily: 'Roboto' }}
+        >
+          <Table.Header>
+            <Table.Row textAlign="center">
+              <Table.HeaderCell width={4} />
+              <Table.HeaderCell>Dishwasher</Table.HeaderCell>
+              <Table.HeaderCell>Line Cook</Table.HeaderCell>
+              <Table.HeaderCell>Executive Chef</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell>Cost</Table.Cell>
+              <Table.Cell textAlign="center">Free</Table.Cell>
+              <Table.Cell textAlign="center">$2/month</Table.Cell>
+              <Table.Cell textAlign="center">$10/year</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>Display Recipes</Table.Cell>
+              <Table.Cell textAlign="center">
+                <Icon name="checkmark" />
+              </Table.Cell>
+              <Table.Cell textAlign="center">
+                <Icon name="checkmark" />
+              </Table.Cell>
+              <Table.Cell textAlign="center">
+                <Icon name="checkmark" />
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>Create/Edit Recipes</Table.Cell>
+              <Table.Cell textAlign="center">
+                <Icon name="checkmark" />
+              </Table.Cell>
+              <Table.Cell textAlign="center">
+                <Icon name="checkmark" />
+              </Table.Cell>
+              <Table.Cell textAlign="center">
+                <Icon name="checkmark" />
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>Copy Recipes</Table.Cell>
+              <Table.Cell textAlign="center">
+                <Icon name="checkmark" />
+              </Table.Cell>
+              <Table.Cell textAlign="center">
+                <Icon name="checkmark" />
+              </Table.Cell>
+              <Table.Cell textAlign="center">
+                <Icon name="checkmark" />
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>Import Recipes</Table.Cell>
+              <Table.Cell textAlign="center">
+                <Icon name="checkmark" />
+              </Table.Cell>
+              <Table.Cell textAlign="center">
+                <Icon name="checkmark" />
+              </Table.Cell>
+              <Table.Cell textAlign="center">
+                <Icon name="checkmark" />
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>Allergy Notifications</Table.Cell>
+              <Table.Cell textAlign="center">
+                <Icon name="checkmark" />
+              </Table.Cell>
+              <Table.Cell textAlign="center">
+                <Icon name="checkmark" />
+              </Table.Cell>
+              <Table.Cell textAlign="center">
+                <Icon name="checkmark" />
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>Nutritional Analysis</Table.Cell>
+              <Table.Cell textAlign="center">
+                <Icon name="close" />
+              </Table.Cell>
+              <Table.Cell textAlign="center">
+                <Icon name="checkmark" />
+              </Table.Cell>
+              <Table.Cell textAlign="center">
+                <Icon name="checkmark" />
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>Select Plan</Table.Cell>
+              <Table.Cell textAlign="center">
+                <Icon name="close" />
+              </Table.Cell>
+              <Table.Cell textAlign="center">
+                <input
+                  type="radio"
+                  id="silver"
+                  checked={this.state.plan === 'silver'}
+                  onChange={() => this.onRadioButton('silver')}
+                />
+              </Table.Cell>
+              <Table.Cell textAlign="center">
+                <input
+                  type="radio"
+                  id="gold"
+                  checked={this.state.plan === 'gold'}
+                  onChange={() => this.onRadioButton('gold')}
+                />
+              </Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
+        <div style={{ width: '95%', marginLeft: '2.5%' }}>
+          <Header as="h3" attached="top" color="violet" inverted>
+            Pay with Card
+          </Header>
+          <Segment attached>
+            <CardElement />
+          </Segment>
+        </div>
         <br />
-        <input
-          type="radio"
-          id="silver"
-          checked={this.state.plan === 'silver'}
-          onChange={() => this.onRadioButton('silver')}
-        />
-        <br />
-        <label htmlFor="gold">Gold Plan: $10.00 per year</label>
-        <br />
-        <input
-          type="radio"
-          id="gold"
-          checked={this.state.plan === 'gold'}
-          onChange={() => this.onRadioButton('gold')}
-        />
-
-        <CardElement />
-        <button onClick={this.submit}>Submit</button>
-        <br />
-        <button onClick={this.props.cancelSubscription}>
+        <Button onClick={this.submit}>Subscribe</Button>
+        <Button color="red" onClick={this.props.cancelSubscription}>
           Cancel Subscription
-        </button>
+        </Button>
       </div>
     );
   }
