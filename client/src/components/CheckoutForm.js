11ec8f92ca58c6501d46dc8a85028929901deb75
Peter Pham <phamhere@gmail.com>
2019-01-23 10:24:03 -0700

    made mobile table in billing

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import { CardElement, injectStripe } from 'react-stripe-elements';
 import { connect } from 'react-redux';
-import { Button, Header, Table, Icon, Segment } from 'semantic-ui-react';
+import {
+  Button,
+  Header,
+  Table,
+  Icon,
+  Segment,
+  Responsive
+} from 'semantic-ui-react';
 
 import { chargeUser, cancelSubscription, getUser } from '../actions';
 
@@ -12,6 +19,290 @@ class CheckoutForm extends React.Component {
 
   onRadioButton = value => {
     this.setState({ plan: value });
+  };
+
+  desktopTable = () => {
+    return (
+      <Table
+        definition
+        striped
+        size="small"
+        color="violet"
+        inverted
+        style={{
+          width: '95%',
+          marginLeft: '2.5%',
+          marginBottom: '15px',
+          fontFamily: 'Roboto'
+        }}
+      >
+        <Table.Header>
+          <Table.Row textAlign="center">
+            <Table.HeaderCell />
+            <Table.HeaderCell>Dishwasher</Table.HeaderCell>
+            <Table.HeaderCell>Line Cook</Table.HeaderCell>
+            <Table.HeaderCell>Executive Chef</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>Cost</Table.Cell>
+            <Table.Cell textAlign="center">Free</Table.Cell>
+            <Table.Cell textAlign="center">$2/month</Table.Cell>
+            <Table.Cell textAlign="center">$10/year</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Display Recipes</Table.Cell>
+            <Table.Cell textAlign="center">
+              <Icon name="checkmark" />
+            </Table.Cell>
+            <Table.Cell textAlign="center">
+              <Icon name="checkmark" />
+            </Table.Cell>
+            <Table.Cell textAlign="center">
+              <Icon name="checkmark" />
+            </Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Create/Edit Recipes</Table.Cell>
+            <Table.Cell textAlign="center">
+              <Icon name="checkmark" />
+            </Table.Cell>
+            <Table.Cell textAlign="center">
+              <Icon name="checkmark" />
+            </Table.Cell>
+            <Table.Cell textAlign="center">
+              <Icon name="checkmark" />
+            </Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Copy Recipes</Table.Cell>
+            <Table.Cell textAlign="center">
+              <Icon name="checkmark" />
+            </Table.Cell>
+            <Table.Cell textAlign="center">
+              <Icon name="checkmark" />
+            </Table.Cell>
+            <Table.Cell textAlign="center">
+              <Icon name="checkmark" />
+            </Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Import Recipes</Table.Cell>
+            <Table.Cell textAlign="center">
+              <Icon name="checkmark" />
+            </Table.Cell>
+            <Table.Cell textAlign="center">
+              <Icon name="checkmark" />
+            </Table.Cell>
+            <Table.Cell textAlign="center">
+              <Icon name="checkmark" />
+            </Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Recipe Reviews</Table.Cell>
+            <Table.Cell textAlign="center">
+              <Icon name="checkmark" />
+            </Table.Cell>
+            <Table.Cell textAlign="center">
+              <Icon name="checkmark" />
+            </Table.Cell>
+            <Table.Cell textAlign="center">
+              <Icon name="checkmark" />
+            </Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Allergy Notifications</Table.Cell>
+            <Table.Cell textAlign="center">
+              <Icon name="checkmark" />
+            </Table.Cell>
+            <Table.Cell textAlign="center">
+              <Icon name="checkmark" />
+            </Table.Cell>
+            <Table.Cell textAlign="center">
+              <Icon name="checkmark" />
+            </Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Nutritional Analysis</Table.Cell>
+            <Table.Cell textAlign="center">
+              <Icon name="close" />
+            </Table.Cell>
+            <Table.Cell textAlign="center">
+              <Icon name="checkmark" />
+            </Table.Cell>
+            <Table.Cell textAlign="center">
+              <Icon name="checkmark" />
+            </Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Select Plan</Table.Cell>
+            <Table.Cell textAlign="center">
+              <Icon name="close" />
+            </Table.Cell>
+            <Table.Cell textAlign="center">
+              <input
+                type="radio"
+                id="silver"
+                checked={this.state.plan === 'silver'}
+                onChange={() => this.onRadioButton('silver')}
+              />
+            </Table.Cell>
+            <Table.Cell textAlign="center">
+              <input
+                type="radio"
+                id="gold"
+                checked={this.state.plan === 'gold'}
+                onChange={() => this.onRadioButton('gold')}
+              />
+            </Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+    );
+  };
+
+  mobileTable = () => {
+    return (
+      <Table
+        definition
+        striped
+        size="small"
+        color="violet"
+        inverted
+        style={{
+          width: '95%',
+          marginLeft: '2.5%',
+          marginBottom: '15px',
+          fontFamily: 'Roboto'
+        }}
+      >
+        <Table.Header>
+          <Table.Row textAlign="center">
+            <Table.HeaderCell style={{ background: '#6435C9'}} />
+            <Table.HeaderCell>Dishwasher</Table.HeaderCell>
+            <Table.HeaderCell>Line Cook</Table.HeaderCell>
+            <Table.HeaderCell>Executive Chef</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>Cost</Table.Cell>
+            <Table.Cell textAlign="center">Dishwasher: Free</Table.Cell>
+            <Table.Cell textAlign="center">Line Cook: $2/month</Table.Cell>
+            <Table.Cell textAlign="center">Executive Chef: $10/year</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Display Recipes</Table.Cell>
+            <Table.Cell textAlign="center">
+              Dishwasher: <Icon name="checkmark" />
+            </Table.Cell>
+            <Table.Cell textAlign="center">
+              Line Cook: <Icon name="checkmark" />
+            </Table.Cell>
+            <Table.Cell textAlign="center">
+              Executive Chef: <Icon name="checkmark" />
+            </Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Create/Edit Recipes</Table.Cell>
+            <Table.Cell textAlign="center">
+              Dishwasher: <Icon name="checkmark" />
+            </Table.Cell>
+            <Table.Cell textAlign="center">
+              Line Cook: <Icon name="checkmark" />
+            </Table.Cell>
+            <Table.Cell textAlign="center">
+              Executive Chef: <Icon name="checkmark" />
+            </Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Copy Recipes</Table.Cell>
+            <Table.Cell textAlign="center">
+              Dishwasher: <Icon name="checkmark" />
+            </Table.Cell>
+            <Table.Cell textAlign="center">
+              Line Cook: <Icon name="checkmark" />
+            </Table.Cell>
+            <Table.Cell textAlign="center">
+              Executive Chef: <Icon name="checkmark" />
+            </Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Import Recipes</Table.Cell>
+            <Table.Cell textAlign="center">
+              Dishwasher: <Icon name="checkmark" />
+            </Table.Cell>
+            <Table.Cell textAlign="center">
+              Line Cook: <Icon name="checkmark" />
+            </Table.Cell>
+            <Table.Cell textAlign="center">
+              Executive Chef: <Icon name="checkmark" />
+            </Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Recipe Reviews</Table.Cell>
+            <Table.Cell textAlign="center">
+              Dishwasher: <Icon name="checkmark" />
+            </Table.Cell>
+            <Table.Cell textAlign="center">
+              Line Cook: <Icon name="checkmark" />
+            </Table.Cell>
+            <Table.Cell textAlign="center">
+              Executive Chef: <Icon name="checkmark" />
+            </Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Allergy Notifications</Table.Cell>
+            <Table.Cell textAlign="center">
+              Dishwasher: <Icon name="checkmark" />
+            </Table.Cell>
+            <Table.Cell textAlign="center">
+              Line Cook: <Icon name="checkmark" />
+            </Table.Cell>
+            <Table.Cell textAlign="center">
+              Executive Chef: <Icon name="checkmark" />
+            </Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Nutritional Analysis</Table.Cell>
+            <Table.Cell textAlign="center">
+              Dishwasher: <Icon name="close" />
+            </Table.Cell>
+            <Table.Cell textAlign="center">
+              Line Cook: <Icon name="checkmark" />
+            </Table.Cell>
+            <Table.Cell textAlign="center">
+              Executive Chef: <Icon name="checkmark" />
+            </Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Select Plan</Table.Cell>
+            <Table.Cell textAlign="center">
+              Dishwasher: <Icon name="close" />
+            </Table.Cell>
+            <Table.Cell textAlign="center">
+              Line cook:{' '}
+              <input
+                type="radio"
+                id="silver"
+                checked={this.state.plan === 'silver'}
+                onChange={() => this.onRadioButton('silver')}
+              />
+            </Table.Cell>
+            <Table.Cell textAlign="center">
+              Executive Chef:{' '}
+              <input
+                type="radio"
+                id="gold"
+                checked={this.state.plan === 'gold'}
+                onChange={() => this.onRadioButton('gold')}
+              />
+            </Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+    );
   };
 
   submit = async ev => {
@@ -34,125 +325,8 @@ class CheckoutForm extends React.Component {
     return (
       <div>
         <Header>Don't Eat That Subscription Plans</Header>
-        <Table
-          definition
-          striped
-          size="small"
-          color="violet"
-          inverted
-          style={{ width: '95%', marginLeft: '2.5%', fontFamily: 'Roboto' }}
-        >
-          <Table.Header>
-            <Table.Row textAlign="center">
-              <Table.HeaderCell />
-              <Table.HeaderCell>Dishwasher</Table.HeaderCell>
-              <Table.HeaderCell>Line Cook</Table.HeaderCell>
-              <Table.HeaderCell>Executive Chef</Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            <Table.Row>
-              <Table.Cell>Cost</Table.Cell>
-              <Table.Cell textAlign="center">Free</Table.Cell>
-              <Table.Cell textAlign="center">$2/month</Table.Cell>
-              <Table.Cell textAlign="center">$10/year</Table.Cell>
-            </Table.Row>
-            <Table.Row>
-              <Table.Cell>Display Recipes</Table.Cell>
-              <Table.Cell textAlign="center">
-                <Icon name="checkmark" />
-              </Table.Cell>
-              <Table.Cell textAlign="center">
-                <Icon name="checkmark" />
-              </Table.Cell>
-              <Table.Cell textAlign="center">
-                <Icon name="checkmark" />
-              </Table.Cell>
-            </Table.Row>
-            <Table.Row>
-              <Table.Cell>Create/Edit Recipes</Table.Cell>
-              <Table.Cell textAlign="center">
-                <Icon name="checkmark" />
-              </Table.Cell>
-              <Table.Cell textAlign="center">
-                <Icon name="checkmark" />
-              </Table.Cell>
-              <Table.Cell textAlign="center">
-                <Icon name="checkmark" />
-              </Table.Cell>
-            </Table.Row>
-            <Table.Row>
-              <Table.Cell>Copy Recipes</Table.Cell>
-              <Table.Cell textAlign="center">
-                <Icon name="checkmark" />
-              </Table.Cell>
-              <Table.Cell textAlign="center">
-                <Icon name="checkmark" />
-              </Table.Cell>
-              <Table.Cell textAlign="center">
-                <Icon name="checkmark" />
-              </Table.Cell>
-            </Table.Row>
-            <Table.Row>
-              <Table.Cell>Import Recipes</Table.Cell>
-              <Table.Cell textAlign="center">
-                <Icon name="checkmark" />
-              </Table.Cell>
-              <Table.Cell textAlign="center">
-                <Icon name="checkmark" />
-              </Table.Cell>
-              <Table.Cell textAlign="center">
-                <Icon name="checkmark" />
-              </Table.Cell>
-            </Table.Row>
-            <Table.Row>
-              <Table.Cell>Allergy Notifications</Table.Cell>
-              <Table.Cell textAlign="center">
-                <Icon name="checkmark" />
-              </Table.Cell>
-              <Table.Cell textAlign="center">
-                <Icon name="checkmark" />
-              </Table.Cell>
-              <Table.Cell textAlign="center">
-                <Icon name="checkmark" />
-              </Table.Cell>
-            </Table.Row>
-            <Table.Row>
-              <Table.Cell>Nutritional Analysis</Table.Cell>
-              <Table.Cell textAlign="center">
-                <Icon name="close" />
-              </Table.Cell>
-              <Table.Cell textAlign="center">
-                <Icon name="checkmark" />
-              </Table.Cell>
-              <Table.Cell textAlign="center">
-                <Icon name="checkmark" />
-              </Table.Cell>
-            </Table.Row>
-            <Table.Row>
-              <Table.Cell>Select Plan</Table.Cell>
-              <Table.Cell textAlign="center">
-                <Icon name="close" />
-              </Table.Cell>
-              <Table.Cell textAlign="center">
-                <input
-                  type="radio"
-                  id="silver"
-                  checked={this.state.plan === 'silver'}
-                  onChange={() => this.onRadioButton('silver')}
-                />
-              </Table.Cell>
-              <Table.Cell textAlign="center">
-                <input
-                  type="radio"
-                  id="gold"
-                  checked={this.state.plan === 'gold'}
-                  onChange={() => this.onRadioButton('gold')}
-                />
-              </Table.Cell>
-            </Table.Row>
-          </Table.Body>
-        </Table>
+        <Responsive minWidth={768}>{this.desktopTable()}</Responsive>
+        <Responsive maxWidth={767}>{this.mobileTable()}</Responsive>
         <div style={{ width: '95%', marginLeft: '2.5%' }}>
           <Header as="h3" attached="top" color="violet" inverted>
             Pay with Card
