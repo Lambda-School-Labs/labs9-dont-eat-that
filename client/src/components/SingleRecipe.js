12b62fdefea342506eeff49a8ed8e1b3a98ecaef
MEBonham <programmer@mckaybonham.com>
2019-01-23 17:42:49 -0700

    Revert "Soften red highlighting of allergic ingredients in single recipe view"
    This reverts commit 83ba18aac4ee316ac2147631287d00d372c6984d.

--- client/src/components/SingleRecipe.js
+++ client/src/components/SingleRecipe.js
@@ -156,10 +156,7 @@ class SingleRecipe extends React.Component {
                 console.log('boolarr', boolArr);
                 if (boolArr.includes(true)) {
                   return (
-                    <li key={ingr.name} style={{ 
-                        background: 'rgba(255,0,0,0.75)',
-                        boxShadow: '0 0 3px rgba(255,0,0,0.75)' 
-                      }}>{`${
+                    <li key={ingr.name} style={{ background: 'red' }}>{`${
                       ingr.quantity
                     } ${ingr.unit ? ingr.unit : ''} ${ingr.name}`}</li>
                   );
