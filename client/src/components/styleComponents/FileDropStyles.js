7bd4938bab9d27b4c71b7447ca3fbccf7e5a994e
vanceleon <vanceleon44@gmail.com>
2019-01-28 18:09:03 -0500

    styledComponents file

--- client/src/components/styleComponents/FileDropStyles.js
+++ client/src/components/styleComponents/FileDropStyles.js
@@ -3,7 +3,7 @@ import ourColors from '../../ColorScheme';
 
 export const DropCard = styled.main`
   display: block;
-  width: 90%;
+  width: 98%;
   height: 200px;
   margin: 100px auto;
   background-color: white;
