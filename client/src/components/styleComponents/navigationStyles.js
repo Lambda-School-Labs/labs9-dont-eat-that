82bb40bc8607aa2ac3ffb67a461a668afa3a9517
vanceleon <vanceleon44@gmail.com>
2019-01-29 13:58:36 -0500

    updated browser file on this branch

--- client/src/components/styleComponents/navigationStyles.js
+++ client/src/components/styleComponents/navigationStyles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
-export const MenuSearch = styled.div `
-    max-width: '300px',
+export const MenuSearch = styled.div `   
+max-width: '300px',
     justify-content: 'flex-end',
+    margin: '0 20px',
 `;
