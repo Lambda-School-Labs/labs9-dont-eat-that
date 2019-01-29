9b0be3e9ecc12f9da4518c903de1a371aca486f3
vanceleon <vanceleon44@gmail.com>
2019-01-29 13:26:47 -0500

    deleted service worker file

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,6 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import Firebase, { FirebaseContext } from './components/firebase';
 import rootReducer from './reducers';
 
@@ -29,7 +28,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
