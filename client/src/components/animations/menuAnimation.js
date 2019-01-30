9ae099a04b412a07ec04ae979e202fdadcd55ae2
vanceleon <vanceleon44@gmail.com>
2019-01-30 13:28:50 -0500

    added animation files

---
+++ client/src/components/animations/menuAnimation.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+// GSAP import will be needed
+
+class MenuAnimation extends Component{
+    constructor() {
+        super();
+        this.state = {  
+            loaderWrap: null,
+            loaderTween: null,
+            toggleMenuBtn: null;
+            toggle: false,
+        }
+    }
+    
+
+}
+export default MenuAnimation;
